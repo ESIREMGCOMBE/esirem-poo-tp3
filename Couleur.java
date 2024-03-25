f3950a17fa606560d02aac80bd21b1cbf74f4321
ESIREMGCOMBE <150357144+ESIREMGCOMBE@users.noreply.github.com>
2024-03-25 16:55:32 +0100

    Update Couleur.java
    ajout constructeur et methode

--- Couleur.java
+++ Couleur.java
@@ -1,6 +1,19 @@
 public enum Couleur {
-    COEUR,
-    TREFFLE,
-    CARREAU,
-    PIQUE    
+    COEUR("Coeur"),
+    TREFFLE("Treffle"),
+    CARREAU("Carreau"),
+    PIQUE("Pique");    
+
+
+private String def_Couleur;
+// Constructeur
+private Couleur(String def_Couleur){
+    this.def_Couleur = def_Couleur;
+}
+
+// Méthode
+public String aff_couleur(){
+    return def_Couleur;
+}
+
 };
