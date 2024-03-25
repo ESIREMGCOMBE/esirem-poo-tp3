3f73708c4bd1e17154f40bda2bd87609933a18a9
ESIREMGCOMBE <150357144+ESIREMGCOMBE@users.noreply.github.com>
2024-03-25 16:56:39 +0100

    ajout constructeur et methode

--- Valeur.java
+++ Valeur.java
@@ -1,4 +1,15 @@
 public enum Valeur {
 AS(1),DEUX(2),TROIS(3),QUATRE(4),CINQ(5),SIX(6),SEPT(7),HUIT(8),NEUF(9),DIX(10),VALET(10),DAME(10),ROI(10);
 
+private int carte_valeur;
+
+//Constructeur
+private Valeur(int carte_valeur){
+    this.carte_valeur = carte_valeur;
+}
+
+//Méthode
+public int aff_valeur(){
+    return carte_valeur;
+}
 };
