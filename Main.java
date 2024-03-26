b17d9e7043d83ea5472512edf85d6cc71d87d1f4
ESIREMGCOMBE <150357144+ESIREMGCOMBE@users.noreply.github.com>
2024-03-26 17:42:11 +0100

    Modif du main pour test de la fonction montre_main. test OK

--- Main.java
+++ Main.java
@@ -5,7 +5,20 @@ public static void main(String[] args){
     c1=new Carte(Valeur.AS,Couleur.CARREAU);
     c2=new Carte(Valeur.CINQ,Couleur.PIQUE);
 
-    System.out.println(c1.affiche_carte() + "\n" + c2.affiche_carte());
+    // System.out.println(c1.affiche_carte() + "\n" + c2.affiche_carte());
 
+    //retourner cartes
+
+    c1.switch_face();
+    c2.switch_face();
+
+    //créer main
+    Main_joueur m1 = new Main_joueur();
+    
+    m1.tire_carte(c1);
+    m1.tire_carte(c2);
+    System.out.println(m1.montre_main());
+
+    
 }
 }
