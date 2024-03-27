bf5dbeafb98428091cfcab2e0b77397c159cc1f1
ESIREMGCOMBE <150357144+ESIREMGCOMBE@users.noreply.github.com>
2024-03-27 17:22:56 +0100

    Modif pour gestion de l'affichage "As de *" au lieu de "1 de *"

--- Carte.java
+++ Carte.java
@@ -1,37 +1,55 @@
 // Classe Carte
 public class Carte {
+    
 
     private Valeur valeur; //enum Valeur
     private Couleur couleur; //enum Couleur
-    private boolean face_visible;
+    public boolean face_visible;
 
-
+  
     // Constructeur
-    public Carte(Couleur couleur, Valeur valeur){
+    public Carte(Valeur valeur,Couleur couleur){
         this.couleur = couleur;
         this.valeur = valeur;
-        face_visible  = true;
+        face_visible  = false;
     }
 
     // Méthodes
+    public int getValeur(){
+        return valeur.get_valeur();
+    }
+
+    public String print_valeur(){
+        // récupère valeur en tant que chaîne 
+        // gestions des AS
+        if(this.valeur==Valeur.AS){
+            return "As";
+             }
+             else{
+                return String.valueOf(this.valeur.get_valeur());
+
+             }
+             
+    }
+    public String affiche_carte(){
+        String chain="";
+
+            chain = chain + this.print_valeur() + " de " + this.couleur.get_couleur();           
+     
+
+
+return chain;
+        }
+
 
     public String getCouleur(){
-        return couleur.aff_couleur();
+        return couleur.get_couleur();
         
     }
-    public int getValeur(){
-        return valeur.aff_valeur();
+
+    public void retourne_carte(){
+        face_visible = !face_visible ;
     }
 
-    public String carte_concat(){
-        String str=" ";
-        if(face_visible){
-            str = str + valeur.aff_valeur() + " de " + couleur.aff_couleur();           
-    }   
-        else{
-            str = "Face cachée";
-        }
 
-return str;
-        }
 };
