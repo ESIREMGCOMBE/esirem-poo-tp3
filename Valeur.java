27c1df0f7c9e3bd94c457677c09ccbc9b6374014
ESIREMGCOMBE <150357144+ESIREMGCOMBE@users.noreply.github.com>
2024-03-27 17:29:38 +0100

    Update Valeur.java pour gestion affichage

--- Valeur.java
+++ Valeur.java
@@ -1,15 +1,24 @@
 public enum Valeur {
-AS(1),DEUX(2),TROIS(3),QUATRE(4),CINQ(5),SIX(6),SEPT(7),HUIT(8),NEUF(9),DIX(10),VALET(10),DAME(10),ROI(10);
+AS(1,"As"),DEUX(2,"Deux"),TROIS(3,"Trois"),QUATRE(4,"Quatre"),
+CINQ(5,"Cinq"),SIX(6,"Six"),SEPT(7,"Sept"),
+HUIT(8,"Huit"),NEUF(9,"Neuf"),DIX(10,"Dix"),
+VALET(10,"Valet"),DAME(10,"Dame"),ROI(10,"Roi");
 
 private int carte_valeur;
+private String string_valeur;
 
 //Constructeur
-private Valeur(int carte_valeur){
+private Valeur(int carte_valeur,String string_valeur){
     this.carte_valeur = carte_valeur;
+    this.string_valeur = string_valeur;
 }
 
 //Méthode
-public int aff_valeur(){
+public int get_valeur(){
     return carte_valeur;
 }
+public String aff_valeur(){
+    return string_valeur;
+}
 };
+
