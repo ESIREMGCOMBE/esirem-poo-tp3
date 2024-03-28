0632ddd2aa42178e92821f84e6f9658c85691d94
ESIREMGCOMBE <150357144+ESIREMGCOMBE@users.noreply.github.com>
2024-03-28 16:31:29 +0100

    restructuration commande alt + MAJ +f

--- Couleur.java
+++ Couleur.java
@@ -2,18 +2,18 @@ public enum Couleur {
     COEUR("Coeur"),
     TREFFLE("Treffle"),
     CARREAU("Carreau"),
-    PIQUE("Pique");    
+    PIQUE("Pique");
 
+    private String def_Couleur;
 
-private String def_Couleur;
-// Constructeur
-private Couleur(String def_Couleur){
-    this.def_Couleur = def_Couleur;
-}
+    // Constructeur
+    private Couleur(String def_Couleur) {
+        this.def_Couleur = def_Couleur;
+    }
 
-// Méthode
-public String aff_couleur(){
-    return def_Couleur;
-}
+    // Méthode
+    public String get_couleur() {
+        return def_Couleur;
+    }
 
 };
