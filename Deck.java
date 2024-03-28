9039a684d1b6442605ecdaa0959ff5a127276362
ESIREMGCOMBE <150357144+ESIREMGCOMBE@users.noreply.github.com>
2024-03-28 16:32:12 +0100

    restructuration commande alt + MAJ +f

--- Deck.java
+++ Deck.java
@@ -4,43 +4,41 @@
 import java.util.Random;
 
 public class Deck extends Main_joueur {
-Random rand = new Random(); // initialisation générateur de nombres aléatoires pour la méthode melanger()
-int tailledeck = 52;
-//methode pour créer le deck
-public void generer(){
-for(Couleur couleur: Couleur.values()){
-    for(Valeur valeur: Valeur.values()){
-        Carte carte = new Carte(valeur,couleur);
-        this.tire_carte(carte);
+    Random rand = new Random(); // initialisation générateur de nombres aléatoires pour la méthode melanger()
+    int tailledeck = 52;
 
-    }
-}
-}
-//methode pour mélanger le deck
-public void melanger(){
-for(int i = cartes.size()-1;i >0;i--){ // on parcourt le deck du haut de la pile au début de pile
-    int pif = rand.nextInt(i);  // génération d'un nombre aléatoire pour chaque indice du deck
-    Carte pifCarte = cartes.get(pif);
-    Carte derCarte = cartes.get(i); // échange de la carte d'indice (i) et celle d'indice (pif)
-    cartes.set(i,pifCarte);
-    cartes.set(pif,derCarte);
-}
-}
+    // methode pour créer le deck
+    public void generer() {
+        for (Couleur couleur : Couleur.values()) {
+            for (Valeur valeur : Valeur.values()) {
+                Carte carte = new Carte(valeur, couleur);
+                this.tire_carte(carte);
 
-// distribuer cartes
-public void  distribuer(Main_joueur J1, int NbCartes){
-    for (int i=0; i<NbCartes;i++){
-                this.donne_carte(cartes.get(0),J1);
-        
+            }
+        }
     }
 
-}
+    // methode pour mélanger le deck
+    public void melanger() {
+        for (int i = cartes.size() - 1; i > 0; i--) { // on parcourt le deck du haut de la pile au début de pile
+            int pif = rand.nextInt(i); // génération d'un nombre aléatoire pour chaque indice du deck
+            Carte pifCarte = cartes.get(pif);
+            Carte derCarte = cartes.get(i); // échange de la carte d'indice (i) et celle d'indice (pif)
+            cartes.set(i, pifCarte);
+            cartes.set(pif, derCarte);
+        }
+    }
+
+    // distribuer cartes
+    public void distribuer(Main_joueur J1, int NbCartes) {
+        for (int i = 0; i < NbCartes; i++) {
+            this.donne_carte(cartes.get(0), J1);
+
+        }
+
+    }
 
 }
-
-
- 
-
 
 
 
